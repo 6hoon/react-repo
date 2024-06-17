00a0abfb4c264e4b7520d883b129c1bfd3421556
Seunghoon <sixyhoon@gmail.com>
2024-06-17 17:20:51 +0900

    0617

--- section01/chapter18-1.js
+++ section01/chapter18-1.js
@@ -14,7 +14,7 @@ arr.forEach(doubleArr);
 newArr.forEach((v) => console.log(v));
 console.log("***************");
 
-// 3. map 활용 (forEach => 배열리턴(새로운 변형) / push를 할 필요없이 새로운 배열을 리턴)
+// 3. map 활용 (forEach 와 같은 형식, 배열리턴(새로운 변형) / push를 할 필요없이 새로운 배열을 리턴)
 // 한 문장일 때는 return을 적어주지 않아야한다
 const arr2 = arr.map((v) => {
   return v * 2;
